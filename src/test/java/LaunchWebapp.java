26b15519a02f14905ebabe4e6af43647d58f7762
Vincent Blouin <vincent.blouin@gmail.com>
2013-07-18 16:12:49 -0400

    delete search index before launch in test mode

--- src/test/java/LaunchWebapp.java
+++ src/test/java/LaunchWebapp.java
@@ -1,11 +1,30 @@
+import org.neo4j.kernel.impl.util.FileUtils;
 import org.triple_brain.service.Launch;
+
+import java.io.File;
+import java.io.IOException;
 
 public class LaunchWebapp {
 
     public static void main(String[] args) throws Exception {
+        deleteSolrIndex();
         Launch.before();
         Launcher launcher = new Launcher();
         launcher.launch();
         Launch.after();
+    }
+
+    private static void deleteSolrIndex(){
+        try{
+            File solrIndexFolder = new File(
+                    "target/maven-shared-archive-resources/solr/data/index/"
+            );
+            FileUtils.deleteRecursively(
+                    solrIndexFolder
+            );
+        }catch(IOException e){
+            System.out.println("Couldn't delete solr index");
+        }
+        System.out.println("Solr index deleted");
     }
 }
